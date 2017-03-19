39e9896e60e210ccc68378ecd45570b31c1aa215
HadrienMP <public@hadrienmp.fr>
2017-03-19 12:38:23 +0100

    Rechargement automatique amélioré

--- nbws.py
+++ nbws.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 
-from flask import Flask, abort
+from flask import Flask, abort, jsonify
 from flask.templating import render_template
 
 from ratp_client import get_next_stop_times
@@ -14,13 +14,19 @@ lines = {
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    lines = OrderedDict()
+    lines["Maison -> Darty"] = get_next_stop_times(
+        "http://www.ratp.fr/horaires/fr/ratp/bus/prochains_passages/PP/B102/102_15_42/R")
+    lines["Darty -> Maison"] = get_next_stop_times(
+        "http://www.ratp.fr/horaires/fr/ratp/bus/prochains_passages/PP/B102/102_6_53/A")
+
+    return render_template("index.html", lines=lines)
 
 
 @app.route("/line/<key>")
 def line(key):
     if key in lines.keys():
-        return render_template("line.html", stop=key, times=get_next_stop_times(lines.get(key)))
+        return jsonify(get_next_stop_times(lines.get(key)))
     else:
         abort(404)
 
