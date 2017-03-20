29e357b3ed689ee4cbac96d8239df093e1ecdc55
Hadrien Mens-Pellen <public@hadrienmp.fr>
2017-03-20 15:43:14 +0000

    Externalisation du javascript

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+    $("#refresh").click(update);
+});
+function update() {
+    $('.line').each(function () {
+        updateLine($(this));
+    });
+}
+function updateLine($line) {
+    var key = $line.find(".description").text();
+    $.get("/line/" + key, function (times) {
+        for (var i = 0; i < times.length; i++) {
+            var $section = $line.find("section").eq(i);
+            console.log(times[i][0] + ', ' + times[i][1]);
+            $section.find(".destination").text(times[i][0]);
+            $section.find(".minutes").text(times[i][1]);
+            $section.find(".time").text(times[i][2]);
+        }
+    });
+}
